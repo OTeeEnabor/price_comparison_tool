3c835e07cc2fe63591a53640b2ca10558197301b
OTeeEnabor <tobienabor@gmail.com>
2024-11-25 14:13:39 +0200

    scrapped products urls

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ if __name__ == "__main__":
     start = time.perf_counter()
 
     # initialize a StoreScraper object
-    store_scraper = StoreScraper("20240717")
+    store_scraper = StoreScraper()
     # get product urls
-    # store_scraper.get_product_urls()
+    store_scraper.get_product_urls()
 
-    # get product data
-    store_scraper.get_product_data()
+    # # get product data
+    # store_scraper.get_product_data()
 
-    # process the raw data
-    store_scraper.process_product_data()
+    # # process the raw data
+    # store_scraper.process_product_data()
 
-    # upload the processed data
-    store_scraper.upload_products() 
+    # # upload the processed data
+    # store_scraper.upload_products() 
 
-    # get finish time
-    finish = time.perf_counter()
+    # # get finish time
+    # finish = time.perf_counter()
     
-    # print duration of script
-    print(f"finished in {round(finish-start,2)} seconds (s)")
+    # # print duration of script
+    # print(f"finished in {round(finish-start,2)} seconds (s)")
