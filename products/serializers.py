2c5a5ea3300df211b61481fc48bdb1005756f752
OTeeEnabor <tobienabor@gmail.com>
2024-11-25 16:27:19 +0200

    added store and category serializers

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,16 @@
 from rest_framework import serializers
-from .models import Products
+from .models import Store, Category, Products
+
+
+class StoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Store
+        fields = "__all__"
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
