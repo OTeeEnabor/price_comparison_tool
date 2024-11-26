21f170a4e50967b6783296b0e55971b0e0b94e36
OTeeEnabor <tobienabor@gmail.com>
2024-11-26 21:40:47 +0200

    added CategoryViewset to URLS.py

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 # from django.conf.urls import include
 from rest_framework.routers import DefaultRouter
-from products.views import ProductsViewset, StoreViewset
+from products.views import CategoryViewset, ProductsViewset, StoreViewset
 
 
 # create router
@@ -11,6 +11,9 @@ router = DefaultRouter()
 router.register("all-products", ProductsViewset, basename="all-products")
 # register store viewset
 router.register("all-stores", StoreViewset, basename="all-stores")
+# register all-stores/{store}/categories
+router.register("categories", CategoryViewset, basename="categories")
+
 urlpatterns = [
     path("", include(router.urls))
     # path("products/", ProductView.as_view(), name="all-products")
